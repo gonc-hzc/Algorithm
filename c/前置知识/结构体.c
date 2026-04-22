12f4196025daa54c4d3a96bb306c604df8730891
Gonc <2498134096@qq.com>
2026-04-22 17:21:32 +0800

    修改了结构体写法说明

--- c/前置知识/结构体.c
+++ c/前置知识/结构体.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
-struct point
+//标准写法
+typedef struct StructureName  //结构体标签名
 {
-    int x;
-    int y;
-};
+    char data;
+    struct  StructureName *p;
+}SiteName;  //结构体别名
+
+/*
+只写前面，不写后面（不用typedef）
+struct StructureName  //结构体标签名
+{
+    char data;
+    struct  StructureName *p;  //标签名可以用来定义指针
+}; 
+
+这种写法定义变量必须加struct：struct StructureName a;
+不能直接写：StructureName a；
+*/
+
+/*
+只写后面，不写前面（匿名结构体）
+typedef struct
+{
+    char data;
+    //没有标签名不能自引用指针
+}SiteName; 别名
+*/
+
 
 int main()
 {
